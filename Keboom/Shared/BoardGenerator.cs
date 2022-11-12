c262d439e797184af14c0ccc52a2d9f1ba0eacc5
Artem Chetverikov <achetver@itron.com>
2022-11-11 17:12:34 -0800

    Add mine generation

---
+++ Keboom/Shared/BoardGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keboom.Shared;
+
+public class BoardGenerator
+{
+    public Board CreateBoard(int width, int height, int numberOfMines)
+    {
+        var board = new Board(width, height);
+
+        var random = new Random();
+
+        for(int mine = 0; mine < numberOfMines; mine++)
+        {
+            while (true)
+            {
+                var x = random.Next(width);
+                var y = random.Next(height);
+
+                if (!board.Grid[x, y].HasMine)
+                {
+                    board.Grid[x, y].HasMine = true;
+                    break;
+                }
+            }
+        }
+
+        return board;
+    }
+}
