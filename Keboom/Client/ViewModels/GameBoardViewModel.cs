9e682087f9f85800f79b4792b18306d974874426
Kenneth White <Kenny.White@intellitect.com>
2022-11-11 20:29:11 -0800

    The greatest board the modern programmer can create

--- Keboom/Client/ViewModels/GameBoardViewModel.cs
+++ Keboom/Client/ViewModels/GameBoardViewModel.cs
@@ -1,5 +1,23 @@
-﻿namespace Keboom.Client.ViewModels;
+﻿using Keboom.Shared;
+
+namespace Keboom.Client.ViewModels;
 
 public class GameBoardViewModel : ViewModelBase
 {
+
+    public GameState? GameState { get; private set; }
+
+    public override Task OnInitializedAsync()
+    {
+        var bg = new BoardGenerator();
+        GameState = new GameState
+        {
+            Board = bg.CreateBoard(10, 10, 22),
+            Player1 = new Player { Id = int.MaxValue, Name = "Player 1", Score = 0 },
+            Player2 = new Player { Id = int.MinValue, Name = "Player 2", Score = 0 }
+        };
+
+        GameState.CurrentPlayer = GameState.Player1;
+        return base.OnInitializedAsync();
+    }
 }
