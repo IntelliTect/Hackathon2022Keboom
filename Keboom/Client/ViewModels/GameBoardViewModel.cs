f0611f2245acab3595d1859f167210911da52d71
Kenneth White <Kenny.White@intellitect.com>
2022-11-11 22:18:15 -0800

    Extended Kelly's life by moving in-line styles to css file

--- Keboom/Client/ViewModels/GameBoardViewModel.cs
+++ Keboom/Client/ViewModels/GameBoardViewModel.cs
@@ -9,8 +9,14 @@ public class GameBoardViewModel : ViewModelBase
 
     public override Task OnInitializedAsync()
     {
+        GameState = new GameState
+        {
+            Board = BoardGenerator.CreateBoard(8, 8, 22),
+            Player1 = new Player { Id = new Guid(), Name = "Player 1", Score = 0 },
+            Player2 = new Player { Id = new Guid(), Name = "Player 2", Score = 0 }
+        };
 
-        //TODO swagger client
+        GameState.CurrentPlayer = GameState.Player1;
         return base.OnInitializedAsync();
     }
 }
