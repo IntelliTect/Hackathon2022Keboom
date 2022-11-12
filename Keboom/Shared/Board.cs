45aa712800811e40d1bb388555cd7af476ab46a8
Artem Chetverikov <achetver@itron.com>
2022-11-11 16:34:39 -0800

    Add Grid Stubs

---
+++ Keboom/Shared/Board.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keboom.Shared
+{
+    public class Board
+    {
+        BoardSpace[,] Grid { get; init; }
+
+        public Board(int width, int height)
+        {
+            Grid = new BoardSpace[width, height];
+        }
+    }
+}
