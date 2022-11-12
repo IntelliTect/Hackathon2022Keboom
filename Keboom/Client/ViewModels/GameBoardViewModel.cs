1b22a28b93da023a5c9dc7fcfc3ffc54d771de2c
Kevin Bost <kitokeboo@gmail.com>
2022-11-12 00:16:29 -0800

    Clicking cells reveals mines

--- Keboom/Client/ViewModels/GameBoardViewModel.cs
+++ Keboom/Client/ViewModels/GameBoardViewModel.cs
@@ -11,9 +11,9 @@ public class GameBoardViewModel : ViewModelBase
     {
         GameState = new GameState
         {
-            Board = BoardGenerator.CreateBoard(8, 8, 22),
-            Player1 = new Player { Id = new Guid(), Name = "Player 1", Score = 0 },
-            Player2 = new Player { Id = new Guid(), Name = "Player 2", Score = 0 }
+            Board = BoardGenerator.CreateBoard(8, 8, 15),
+            Player1 = new Player { Id = Guid.NewGuid(), Name = "Player 1", Score = 0 },
+            Player2 = new Player { Id = Guid.NewGuid(), Name = "Player 2", Score = 0 }
         };
 
         GameState.CurrentPlayer = GameState.Player1;
