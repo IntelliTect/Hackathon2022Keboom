15e794409574fdb4448241bd2c141c42346c4eb1
Artem Chetverikov <achetver@itron.com>
2022-11-11 18:38:56 -0800

    Add adjacency calculation

---
+++ Keboom.Shared.Tests/BoardTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keboom.Shared.Tests;
+
+public class BoardTests
+{
+    [Fact]
+    public void GetAdjacentCount_NoAdjacent_0()
+    {
+        var board = new Board(3, 3);
+
+        var hasMine = board.GetAdjacentCount(1, 1);
+
+        Assert.Equal(0, hasMine);
+    }
+
+    [Fact]
+    public void GetAdjacentCount_4Adjacent_4()
+    {
+        var board = new Board(3, 3);
+        board.Grid[0, 0].HasMine = true;
+        board.Grid[1, 0].HasMine = true;
+        board.Grid[2, 0].HasMine = true;
+        board.Grid[0, 1].HasMine = true;
+
+        var hasMine = board.GetAdjacentCount(1, 1);
+
+        Assert.Equal(4, hasMine);
+    }
+
+    [Fact]
+    public void GetAdjacentCount_3Corner_3()
+    {
+        var board = new Board(3, 3);
+        board.Grid[1, 0].HasMine = true;
+        board.Grid[1, 1].HasMine = true;
+        board.Grid[0, 1].HasMine = true;
+
+        var hasMine = board.GetAdjacentCount(0, 0);
+
+        Assert.Equal(3, hasMine);
+    }
+}
--- Keboom/Shared/Board.cs
+++ Keboom/Shared/Board.cs
@@ -2,10 +2,65 @@
 
 public class Board
 {
-    public BoardSpace[,] Grid { get; init; }
+    public BoardSpace[,] Grid { get; }
+    public int Width { get; }
+    public int Height { get; }
 
     public Board(int width, int height)
     {
         Grid = new BoardSpace[width, height];
+        Width = width;
+        Height = height;
+
+        for(var x = 0; x < width; x++)
+        {
+            for(var y = 0; y < height; y++)
+            {
+                Grid[x, y] = new BoardSpace();
+            }
+        }
+    }
+
+    public int GetAdjacentCount(int x, int y)
+    {
+        var count = 0;
+
+        if (x > 0)
+        {
+            count += Grid[x - 1, y].HasMine ? 1 : 0;
+        }
+        if (x < Width - 1)
+        {
+            count += Grid[x + 1, y].HasMine ? 1 : 0;
+        }
+
+        if (y > 0)
+        {
+            count += Grid[x, y - 1].HasMine ? 1 : 0;
+        }
+        if (y < Height - 1)
+        {
+            count += Grid[x, y + 1].HasMine ? 1 : 0;
+        }
+
+        if (x > 0 && y > 0)
+        {
+            count += Grid[x - 1, y - 1].HasMine ? 1 : 0;
+        }
+        if (x > 0 && y < Height - 1)
+        {
+            count += Grid[x - 1, y + 1].HasMine ? 1 : 0;
+        }
+
+        if (x < Width - 1 && y > 0)
+        {
+            count += Grid[x + 1, y - 1].HasMine ? 1 : 0;
+        }
+        if (x < Width - 1 && y < Height - 1)
+        {
+            count += Grid[x + 1, y + 1].HasMine ? 1 : 0;
+        }
+
+        return count;
     }
 }
