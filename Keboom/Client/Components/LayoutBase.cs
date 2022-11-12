76f2dcf76f26253e779ce7ac1c170e513cb7bc29
Kenneth White <Kenny.White@intellitect.com>
2022-11-11 17:26:15 -0800

    Revert "Revert "Cleaned up bootstrap stuff, created empty landing page""
    This reverts commit d054dd090dc8a27dbe6041a6d0c714c20d26e15a.

---
+++ Keboom/Client/Components/LayoutBase.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Keboom.Client.Components
+{
+    public class LayoutBase : LayoutComponentBase
+    {
+        [Parameter]
+        public string? PageTitle { get; set; } = "Minesweeper";
+    }
+}
