d940b9e7b5cfd5eed072756ec3b477576f90854f
Kevin Bost <kevin@intellitect.com>
2022-11-16 21:26:01 -0800

    Link back to intellitect
    Fixes #44

--- Keboom/Client/Components/LayoutBase.cs
+++ Keboom/Client/Components/LayoutBase.cs
@@ -1,10 +1,9 @@
 ﻿using Microsoft.AspNetCore.Components;
 
-namespace Keboom.Client.Components
+namespace Keboom.Client.Components;
+
+public class LayoutBase : LayoutComponentBase
 {
-    public class LayoutBase : LayoutComponentBase
-    {
-        [Parameter]
-        public string? PageTitle { get; set; } = "KeBOOM";
-    }
+    [Parameter]
+    public string? PageTitle { get; set; } = "KeBOOM";
 }
